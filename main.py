b3bf15a538c6627331b7d1eff08f5b96b8e83053
Fernando <caetanof@alunos.utfpr.edu.br>
2026-02-20 15:33:08 -0300

    add spatial resolution calibration routine

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import tqdm
+import cv2
 from visual_odometer import VisualOdometer
 
 from post_processing.utils.ensaio import EnsaioReader
@@ -73,6 +74,70 @@ def process_ensaio(path):
     return trajectory, displacements, quaternions, timestamps
 
 
+def find_circle_and_bbox(frame, min_radius=0, max_radius=0):
+    gray = cv2.medianBlur(frame, 5)
+
+    circles = cv2.HoughCircles(
+        gray,
+        cv2.HOUGH_GRADIENT_ALT,
+        dp=1,
+        minDist=20,
+        param1=300,
+        param2=0.90,
+        minRadius=min_radius,
+        maxRadius=max_radius,
+    )
+
+    if circles is not None:
+        x, y, r = circles[0][0]
+        d = 2 * r
+
+        # bounding box
+        top_left = (x - r, y - r)
+        bottom_right = (x + r, y + r)
+
+        cv2.circle(frame, (int(round(x)), int(round(y))), int(round(r)), (0, 255, 0), 1)
+        cv2.rectangle(
+            frame,
+            np.int32(np.around(top_left)),
+            np.int32(np.around(bottom_right)),
+            (0, 0, 255),
+            1,
+        )
+
+        return float(d), float(d), float(r), frame
+    else:
+        return None, None, None, frame
+
+
+def calibrate_spatial_resolution(path):
+    ensaio = EnsaioReader(path)
+    imgs = ensaio.get_all_imgs()
+    avg_img = np.zeros_like(ensaio.get_img(0)[1], dtype=np.float32)
+    avg_size = 0
+
+    for i, (timestamp, img) in tqdm.tqdm(
+        enumerate(imgs), desc=f"{path.stem}", total=len(imgs)
+    ):
+        width, height, radius, output_img = find_circle_and_bbox(
+            ensaio.get_img(0)[1], min_radius=12, max_radius=600
+        )
+
+        avg_img += output_img
+
+        if width and height:
+            avg_size += 2 * radius
+
+    avg_img /= len(imgs)
+    avg_size /= len(imgs)
+
+    plt.axis("off")
+    plt.title(f"Estimated circle diameter = {avg_size:.3f} px")
+    plt.imshow(avg_img)
+    plt.savefig(path.with_suffix(".jpg"))
+    plt.close()
+
+
 def try_load(path):
     if path.is_file():
         data = np.load(path, allow_pickle=True)
@@ -106,24 +171,28 @@ def main(args):
                 path = root / pathlib.Path(file)
 
                 try:
-                    data = try_load(path.with_suffix(".npz"))
+                    if args.calibration:
+                        calibrate_spatial_resolution(path)
 
-                    if not data or args.force_processing:
-                        trajectory, displacements, quaternions, timestamps = (
-                            process_ensaio(path)
-                        )
-                        save(
-                            path.with_suffix(".npz"),
-                            trajectory,
-                            displacements,
-                            quaternions,
-                            timestamps,
-                        )
                     else:
-                        print(f"Found existing cache for {path.stem}, using it...")
-                        trajectory, displacements, quaternions, timestamps = data
+                        data = try_load(path.with_suffix(".npz"))
 
-                    plot_displacements_2d(path, trajectory, abs(displacements))
+                        if not data or args.force_processing:
+                            trajectory, displacements, quaternions, timestamps = (
+                                process_ensaio(path)
+                            )
+                            save(
+                                path.with_suffix(".npz"),
+                                trajectory,
+                                displacements,
+                                quaternions,
+                                timestamps,
+                            )
+                        else:
+                            print(f"Found existing cache for {path.stem}, using it...")
+                            trajectory, displacements, quaternions, timestamps = data
+
+                        plot_displacements_2d(path, trajectory, abs(displacements))
 
                 except Exception as e:
                     print(f"Error processing {path.stem}: {e}")
