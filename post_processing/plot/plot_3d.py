fix quaternion ordering to scalar first

post_processing/plot/plot_3d.py
@@ -6,7 +6,7 @@ from scipy.spatial.transform import Rotation as R
 
 from post_processing.utils.imu_tools import fast_rot, rotate_vector
 
-def plot_3d(args, path, list_displacements, list_quaternions, px_p_mm):
+def plot_3d(args, path, list_displacements, list_quaternions, spatial_resolution):
     # Lista com as variações de deslocamento
     variations = [
         # [1, 2],
@@ -83,7 +83,7 @@ def plot_3d(args, path, list_displacements, list_quaternions, px_p_mm):
                 raise Exception("function calculate_and_plot invalid method")
 
             # Acumular o deslocamento
-            position += displacement_3d
+            position += displacement_3d / spatial_resolution
 
             # Armazenar a posição acumulada
             trajectory.append(position.copy())
@@ -113,7 +113,7 @@ def plot_3d(args, path, list_displacements, list_quaternions, px_p_mm):
         ax.legend()
 
         # Mostrar o deslocamento total (última posição) em mm
-        final_position = position * px_p_mm
+        final_position = position
         ax.text(
             final_position[0], final_position[1], final_position[2],
             f'({final_position[0]:.1f} mm, {final_position[1]:.1f} mm, {final_position[2]:.1f} mm)',

post_processing/processing/displacements.py
@@ -26,7 +26,7 @@ def compute_displacements(ensaio: EnsaioReader):
 
     imgs = ensaio.get_all_imgs()
 
-    unmatched_quaternions = np.array([[data["timestamp"], data["qx"], data["qy"], data["qz"], data["qw"]] for data in ensaio.get_imu_data()])
+    unmatched_quaternions = np.array([[data["timestamp"], data["qw"], data["qx"], data["qy"], data["qz"]] for data in ensaio.get_imu_data()])
     quaternions = []
     timestamps = []
 