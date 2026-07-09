170901ba54a1ab9b96084a3aa432740be2f153c8
Fernando <caetanof@alunos.utfpr.edu.br>
2026-07-09 14:28:28 -0300

    naive quaternion interpolation

--- post_processing/processing/displacements.py
+++ post_processing/processing/displacements.py
@@ -7,30 +7,34 @@ from tqdm import tqdm
 from visual_odometer import VisualOdometer
 from post_processing.utils.ensaio import EnsaioReader
 
-def _lower_bound(arr, val):
-    if len(arr) <= 1:
-        return 0
 
-    mid = len(arr)//2
+def _upper_bound(arr, val):
+    # finds first element in an ordered array such that arr[i] > val
+    def _helper(first, last):
+        if first == last:
+            return first
 
-    if arr[mid] > val:
-        return _lower_bound(arr[mid+1:], val) + mid
+        mid = (first + last) // 2
 
-    else:
-        return _lower_bound(arr[:mid+1], val)
+        if arr[mid] > val:
+            return _helper(first, mid)
+
+        return _helper(mid + 1, last)
+
+    return _helper(0, len(arr) - 1)
 
 
 def _interpolate_quaternion(timestamp, quaternions):
-    i = _lower_bound(quaternions[:,0], timestamp)
+    i = _upper_bound(quaternions[:, 0], timestamp)
 
-    t0, q0 = quaternions[i,0], quaternions[i,1:]
+    t1, q1 = quaternions[i, 0], quaternions[i, 1:]
 
-    if len(quaternions) > i+2:
-        t1, q1 = quaternions[i+1,0], quaternions[i+1,1:]
+    if i >= 1:
+        t0, q0 = quaternions[i - 1, 0], quaternions[i - 1, 1:]
         return (timestamp - t0) * (q1 - q0) / (t1 - t0) + q0
 
-    return q0
-    
+    return q1
+
 
 def compute_displacements(ensaio: EnsaioReader):
     odometer = VisualOdometer(
@@ -49,7 +53,12 @@ def compute_displacements(ensaio: EnsaioReader):
 
     Thread(target=_pub_imgs, daemon=True).start()
 
-    unmatched_quaternions = np.array([[data["timestamp"], data["qw"], data["qx"], data["qy"], data["qz"]] for data in ensaio.get_imu_data()])
+    unmatched_quaternions = np.array(
+        [
+            [data["timestamp"], data["qw"], data["qx"], data["qy"], data["qz"]]
+            for data in ensaio.get_imu_data()
+        ]
+    )
     quaternions = []
     timestamps = []
 
@@ -67,7 +76,6 @@ def compute_displacements(ensaio: EnsaioReader):
         timestamps.append(timestamp)
 
         quaternions.append(_interpolate_quaternion(timestamp, unmatched_quaternions))
-        
 
     displacements = np.array(displacements)
     quaternions = np.array(quaternions)
